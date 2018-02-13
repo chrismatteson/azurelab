a421a2679d0f3facb08ef182aae30322b0ff8263
chrismatteson <chris.matteson@gmail.com>
2018-02-13 06:44:18 -0800

    Update variables.tf

--- exercise3/variables.tf
+++ exercise3/variables.tf
@@ -1,3 +1,19 @@
+variable "client_id" {
+  type        = "string"
+}
+
+variable "client_secret" {
+  type        = "string"
+}
+
+variable "tenant_id" {
+  type        = "string"
+}
+
+variable "subscription_id" {
+  type        = "string"
+}
+
 variable "resource_group_name" {
   type        = "string"
   default     = "myfirstresourcegroup"
