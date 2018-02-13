27ed8df7912e5729d1761aebf687defc27be3893
chrismatteson <chris.matteson@gmail.com>
2018-02-13 06:40:33 -0800

    Update main.tf

--- exercise3/main.tf
+++ exercise3/main.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+  client_id = "${var.client_id}"
+  tenant_id = "${var.tenant_id}"
+  client_secret = "${var.client_secret}"
+  subscription_id = "${var.subscription_id}"
+}
+
 resource "azurerm_resource_group" "myfirstrg" {
   name     = "${var.resource_group_name}"
   location = "${var.resource_group_location}"
